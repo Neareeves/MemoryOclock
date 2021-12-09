ff71a0e6f1b152953f014fbebea54b50455c8ea8
Neareeves <alethesj@gmail.com>
2021-12-09 10:38:34 +0100

    Ajout du fichier de variables de connexion anonymisé

---
+++ App/Models/database.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Ce fichier permet de mettre à part ses mots de passe sans les divulguer en les pushant sur git
+ * Ajoutez les identifiants de votre propre base de données
+ */
+$host ='127.0.0.1';
+$db = '';
+$user = '';
+$charset = 'utf8mb4';
+$pass = '';
